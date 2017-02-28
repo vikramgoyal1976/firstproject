3a4699f1aaf0075375e1d2ead170c045651a91e1
Vikram Goyal <vikram.vkgoyal@gmail.com>
2017-02-28 09:05:47 +0530

    Added File Distance.py

---
+++ Distance.py
@@ -0,0 +1,14 @@
+class Distance:
+	def __init__(self,size, value, threshold):
+		this.value=[]
+		size=0
+		threshold=0
+	
+	def DistancePrimary(Points, threshold):
+		this.size = Points.size
+		this.threshold = threshold
+		thre = threshold * threshold
+		location=0
+		for 
+		
+		
